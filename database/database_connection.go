86458580b1f1fb76ebe80df9709b604fc7c34737
sinqx <sinqxpls@gmail.com>
2023-05-05 16:28:55 +0600

    Change connection

--- database/database_connection.go
+++ database/database_connection.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"os"
 	"sg/models"
 	"time"
 )
 
 func Connect() *gorm.DB {
-	username := "postgres"
-	password := ""
-	dbName := "postgres"
-	dbHost := "db"
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
 	dbType := "postgres"
-	dbPort := "5432"
+	dbPort := os.Getenv("DB_PORT")
 
-	dbUri := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, username,
-		password, dbName, dbPort) // connection string
+	dbUri := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, dbHost, dbPort, dbName)
 
 	database, err := gorm.Open(dbType, dbUri)
 	if err != nil {
